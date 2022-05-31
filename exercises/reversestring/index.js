a7373d7092b0ad995a33b665dda022518f98c3d7
Justin Goncalves <jvgoncalves85@gmail.com>
2022-05-31 19:53:20 -0400

    Added solutions to reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,47 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// function reverse(str) {
+//     return str 
+//     .split('')
+//     .reverse()
+//     .join('');
+// }
+// reverse('hello')
+
+// function reverse(str) {
+//     let reversed = "";
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i];
+//     }
+//     return reversed;
+// }
+// reverse('hello');
+
+// function reverse(str) {
+//     let reversed = '';
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+//     return(reversed);
+// }
+// reverse('hello')
+
+function reverse(str) {
+    return str.split('').reduce((rev, char) => char + rev, '');
+  }
+reverse('hello')
+
+// function reverse(str){
+//     if(!str || typeof str != 'string' || str.length < 2 ) return str;
+    
+//     const backwards = [];
+//     const totalItems = str.length - 1;
+//     for(let i = totalItems; i >= 0; i--){
+//       backwards.push(str[i]);
+//     }
+//     return backwards.join('');
+//   }
+//   reverse('hello')
 
 module.exports = reverse;
